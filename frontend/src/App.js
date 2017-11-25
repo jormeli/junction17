4bc83cd0076bf1aed70afddfad813fe040b8d720
Antti <anttis.pitkanen@gmail.com>
2017-11-25 20:02:19 +0200

    boilerplate continues

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,30 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import createSagaMiddleware from 'redux-saga';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
+import './resources/styles/styles.scss';
+
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(
+  () => {},
+  composeWithDevTools(
+    applyMiddleware(sagaMiddleware)
+  )
+);
+
+// TODO: run saga
+
 const App = () => (
-  <div className="App">
-    <header className="App-header">
-      <h1 className="App-title">Welcome to React</h1>
-    </header>
-    <p className="App-intro">
-      To get started, edit <code>src/App.js</code> and save to reload.
-    </p>
-  </div>
+  <Provider store={{}}>
+    <Router>
+      <div className="page-wrap">
+        <Route exact path="/" component={() => <div>jee</div>} />
+      </div>
+    </Router>
+  </Provider>
 );
 
 export default App;
