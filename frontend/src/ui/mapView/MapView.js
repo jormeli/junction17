4791443925d4bdb1b93b33c189262519c6f05c8a
Kenneth Blomqvist <kekeblom@gmail.com>
2017-11-26 10:51:36 +0200

    style fixes

--- frontend/src/ui/mapView/MapView.js
+++ frontend/src/ui/mapView/MapView.js
@@ -89,7 +89,12 @@ class MapView extends Component {
                 </div>
 
                 <div className="map-right-column">
-                    <div className="map-camera-video"></div>
+                        <div style={{'textAlign': 'center'}}>
+                            <video width="500" height="800" autoplay="autoplay" loop="loop" style={{margin: 'auto' }}>
+                            <source src="/video.mov" type="video/mp4" />
+                            Your browser does not support the video tag.
+                            </video>
+                        </div>
 
                     <div className="map-camera-stats">
 
